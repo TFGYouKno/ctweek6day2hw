c1bb366807745a4a0364611f140770145fe73b4f
TFGYouKno <brockhoffharrison@comcast.net>
2024-07-18 20:10:08 -0400

    final commit

--- fitness.py
+++ fitness.py
@@ -65,7 +65,7 @@ def get_members():
                 return members_schema.jsonify(members)
     else:
         return jsonify({"error": "Database connection failed"}), 500
-    
+  #for PUT request, make sure to include the member ID in the url and send as JSON format  
 @app.route('/members/<int:id>', methods=['PUT'])
 def update_member(id):
     try:
@@ -84,10 +84,10 @@ def update_member(id):
             if not member:
                 return jsonify({"error": "Member not found"}), 404
             
-            update_member = (member_name['member_name'], id)
+            updated_member = (member_name['member_name'], id)
 
             query = "UPDATE Members SET member_name = %s WHERE id = %s"
-            cursor.execute(query, update_member)
+            cursor.execute(query, updated_member)
             conn.commit()
 
             return jsonify({"message": f"Member successfully updated!{id}"}), 200
@@ -99,7 +99,105 @@ def update_member(id):
     else:
         return jsonify({"error": "Database connection failed"}), 500
 
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    conn = connection()
+    if conn is not None:
+        try:
+            cursor=conn.cursor()
 
+            check_query = "SELECT * FROM Members WHERE id = %s"
+            cursor.execute(check_query, (id,))
+            member = cursor.fetchone()
+            if not member:
+                return jsonify({"error": "Member not found"}), 404
+
+            query = "DELETE FROM Members WHERE id = %s"
+            cursor.execute(query, (id,))
+            conn.commit()
+
+            return jsonify({"message": f"Member successfully deleted!{id}"}), 200
+        except Error as e:
+            return jsonify(e.messages), 500
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
+   #allows to get specific member by id 
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    conn = connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM Members WHERE id = %s", (id,))
+            member = cursor.fetchone()
+            if not member:
+                return jsonify({"error": "Member not found"}), 404
+            else:
+                return member_schema.jsonify(member)
+        finally:
+            if conn and conn.is_connected():
+                cursor.close()
+                conn.close()
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
+    
+    class MemberSchema(ma.Schema):
+        id = fields.Int(dump_only= True)
+        member_id = fields.Int(dump_only= True)
+        focal_point = fields.String()
+        length = fields.Int()
+
+    class Meta:
+        fields = ("member_id", "focal_point", "length")
+
+workout_schema = MemberSchema()
+workouts_schema = MemberSchema(many= True)
+
+@app.route("/workouts", methods = ['GET'])
+def get_workout_sessions():
+    conn = connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor(dictionary= True)
+
+            query = 'SELECT * FROM workoutSessions;'
+
+            cursor.execute(query)
+
+            workouts = cursor.fetchall()
+        finally:
+            if conn and conn.is_connected():
+                cursor.close()
+                conn.close()
+                return workouts_schema.jsonify(workouts)
+    else:
+        return jsonify({"error": "Databse connection failed"}), 500
+    
+
+@app.route("/workouts/<int:id>", methods = ["PUT"])
+def find_members(id):
+    try:
+        member_data = member_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    conn = connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            
+            check_query = "SELECT * FROM members WHERE id = %s"
+            cursor.execute(check_query, (id,))
+            member = cursor.fetchone()
+            if not member:
+                return jsonify({"error": "Member not found"}), 404
+        finally :
+            cursor.close()
+            conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
